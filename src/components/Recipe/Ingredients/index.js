8b28e799c31b2ccd3357fe985c122bb194dcd2f3
Corxys <jennblngr@gmail.com>
2021-01-20 11:19:30 +0100

    feat(ingredients): added JSX in the Ingredients component (without style)

--- src/components/Recipe/Ingredients/index.js
+++ src/components/Recipe/Ingredients/index.js
@@ -4,9 +4,58 @@ import './styles.scss';
 
 const Ingredients = () => {
   return (
-    <>
-    TODO: ici les ingrédients de la recette
-    </>
+    <div className="recipe__block-right__ingredients">
+      <h2 className="recipe__block-right__ingredients__title">
+        Ingrédients
+      </h2>
+      <div className="recipe__block-right__ingredients">
+        <div className="recipe__block-right__ingredient">
+          <img className="recipe__block-right__ingredient__img" />
+          <div className="recipe__block-right__ingredient__title">
+            Nom
+          </div>
+          <div className="recipe__block-right__ingredient__quantity">
+            Unité
+          </div>
+        </div>
+        <div className="recipe__block-right__ingredient">
+          <img className="recipe__block-right__ingredient__img" />
+          <div className="recipe__block-right__ingredient__title">
+            Nom
+          </div>
+          <div className="recipe__block-right__ingredient__quantity">
+            Unité
+          </div>
+        </div>
+        <div className="recipe__block-right__ingredient">
+          <img className="recipe__block-right__ingredient__img" />
+          <div className="recipe__block-right__ingredient__title">
+            Nom
+          </div>
+          <div className="recipe__block-right__ingredient__quantity">
+            Unité
+          </div>
+        </div>
+        <div className="recipe__block-right__ingredient">
+          <img className="recipe__block-right__ingredient__img" />
+          <div className="recipe__block-right__ingredient__title">
+            Nom
+          </div>
+          <div className="recipe__block-right__ingredient__quantity">
+            Unité
+          </div>
+        </div>
+        <div className="recipe__block-right__ingredient">
+          <img className="recipe__block-right__ingredient__img" />
+          <div className="recipe__block-right__ingredient__title">
+            Nom
+          </div>
+          <div className="recipe__block-right__ingredient__quantity">
+            Unité
+          </div>
+        </div>
+      </div>
+    </div>
 
   )
 };
