87cdeef6d2d22e146b69e94e1bb3caaaecb807a1
SpookyUmi <chloe.danisevicius@gmail.com>
2021-01-20 12:02:25 +0100

    style: add a bit of style to each component

--- src/components/Generator/index.js
+++ src/components/Generator/index.js
@@ -3,14 +3,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // == Import
+import './styles.scss';
 
 // == Composant
 const Generator = () => {
 
-
   return (
-    <div className="generator_container">
-
+    <div className="generator">
+      <h2 className="generator__title">Le Générateur</h2>
+      <section className="generator__section">
+        <div className="generator__section__elem"></div>
+        <button>Try me !</button>
+      </section>
 
     </div>
   );
--- src/components/Home/HomePresentation/index.js
+++ src/components/Home/HomePresentation/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // == Import
-
+import photo from 'src/assets/images/cooking.png';
 import '../styles.scss';
 
 // == Composant
@@ -11,14 +11,18 @@ const HomePresentation = ({ title }) => {
 
   return (
     <div className="home__presentation">
-      <section className="home__presentation__elem">
+      <section className="home__presentation__elem home__presentation__text">
         <h2>Gérez votre quotidien avec {title}</h2>
         <p>Moderne et ludique, O'Fourneaux vous accompagne au quotidien
         en vous proposant des recettes variées, en fonction de la saison
         et de vos exigences alimentaires.</p>
         <button>S'inscrire</button>
       </section>
-      <img href="/public/img/perfect_meme.png" className="home__presentation__elem"/>
+      <section className="home__presentation__elem">
+        <img src={photo} className="home__presentation__img"/>
+      </section>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffdfb1" fillOpacity="1" d="M0,96L80,122.7C160,149,320,203,480,192C640,181,800,107,960,90.7C1120,75,1280,117,1360,138.7L1440,160L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+      </svg>
     </div>
   );
 };
