eacd597720841b1650ec379294b61e0666d0bcb4
Corxys <jennblngr@gmail.com>
2021-01-21 13:31:32 +0100

    refactor(recipe/index.js): deletion of the fragment in the Recipe component

--- src/components/Recipe/index.js
+++ src/components/Recipe/index.js
@@ -11,19 +11,17 @@ const Recipe = ({recipes}) => {
   // console.log(recipes);
 
   return (
-    <>
-      <div className="recipe">
-        <div className="recipe__block recipe__block--left" >
-          <Informations recipe={recipes[0]} />
-          <Instructions instructions={recipes[0].instructions} />
-        </div>
-
-        <div className="recipe__block recipe__block--right" >
-          <img className="recipe__img" src={recipes[0].picture} />
-          <Ingredients ingredients={recipes[0].ingredients} />
-        </div>
+    <div className="recipe">
+      <div className="recipe__block recipe__block--left" >
+        <Informations recipe={recipes[0]} />
+        <Instructions instructions={recipes[0].instructions} />
       </div>
-    </>
+
+      <div className="recipe__block recipe__block--right" >
+        <img className="recipe__img" src={recipes[0].picture} />
+        <Ingredients ingredients={recipes[0].ingredients} />
+      </div>
+    </div>
   )
 };
 
