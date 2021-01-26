5a7b637f348653a1eac622004a1b1f01c8dc8bbe
Corxys <jennblngr@gmail.com>
2021-01-26 18:00:23 +0100

    refactor(header/index.js): eSLint error correction

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,57 +1,47 @@
-// == Import npm
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { NavLink, Redirect } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-// == Import
 import './styles.scss';
 
-// == Composant
-const Header = ({ title, isLogged }) => {
-  return (
-    <header className="header">
-      <div className="header__container">
-        {/* TODO: onSubmit, send a GET request (axios), and redirect to /recettes */}
-        <input type="text" placeholder="Recherche..." className="header__container__elem header__container__elem--input desktop"/>
-        <NavLink exact to="/">
-          <h1 className="header__container__title">{title}</h1>
-        </NavLink>
-        { !isLogged &&
-          <div className="header__container__buttons desktop">
-        {/* TODO: Link to /inscription */}
+const Header = ({ title, isLogged }) => (
+  <header className="header">
+    <div className="header__container">
+      {/* TODO: onSubmit, send a GET request (axios), and redirect to /recettes */}
+      <input type="text" placeholder="Recherche..." className="header__container__elem header__container__elem--input desktop"/>
+      <NavLink exact to="/">
+        <h1 className="header__container__title">{title}</h1>
+      </NavLink>
+      { !isLogged && (
+        <div className="header__container__buttons desktop">
           <NavLink exact to="/inscription" className="header__container__elem button__style">S'inscrire</NavLink>
-        {/* TODO: Link to /connexion */}
           <NavLink exact to="/connexion" className="header__container__elem header__container__elem--signin">Se connecter</NavLink>
         </div>
-        }
-        {
-          isLogged &&
-          <div className="header__container__buttons desktop">
-          {/* TODO: Link to /profil */}
-            <NavLink exact to="/profil" className="header__container__elem button__style">Mon profil</NavLink>
+      )}
+      {
+        isLogged && (
+        <div className="header__container__buttons desktop">
+          <NavLink exact to="/profil" className="header__container__elem button__style">Mon profil</NavLink>
           {/* TODO: onClick, toggle isLoggedIn to false + redirect to /home */}
-            <NavLink to="/" className="header__container__elem">Se déconnecter</NavLink>
-          </div>
-        }
-      </div>
-    </header>
-  );
-};
+          <NavLink to="/" className="header__container__elem">Se déconnecter</NavLink>
+        </div>
+        )}
+    </div>
+  </header>
+);
 
-Header.proptypes ={
+Header.propTypes = {
   title: PropTypes.string.isRequired,
   isLogged: PropTypes.bool.isRequired,
-}
-
-const mapStateToProps = (state) => {
-  return {
-    isLogged: state.auth.isLogged,
-  }
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {}
-};
+const mapStateToProps = (state) => ({
+  isLogged: state.auth.isLogged,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
 
 export default connect(mapStateToProps, null)(Header);
