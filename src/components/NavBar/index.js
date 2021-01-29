bcb63d4eabeb39892956f5f2a6d1a10b811aecd6
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-01-29 09:53:40 +0100

    feat(navbar): almost finished style and add state to handle search

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { NavLink, withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import PropTypes from 'prop-types';
 
 import home from 'src/assets/icons/home.svg';
@@ -17,16 +18,17 @@ import about from 'src/assets/icons/about.svg';
 
 import './styles.scss';
 
-const Navbar = ({ isLogged, isAdmin, isOpen, setIsOpen }) => {
+const Navbar = ({ isLogged, isAdmin, isOpen, setIsOpen, trackSearch, handleSearch }) => {
 
   return (
-    <>
+      <>
         {isOpen &&
         <div className="navbar">
-          <img className="navbar__toggle__home__icon" src={arrow} alt="icône flèche gauche"
+          <div className="navbar__links__top">
+          <img className="navbar__toggle__home__icon link__style" src={arrow} alt="icône flèche gauche"
             onClick={() => { setIsOpen(false) }}
               />
-          <div className="navbar__searchform">
+          <form onSubmit={handleSearch} className="navbar__searchform">
             <div className="navbar__searchform__icon">
               <FontAwesomeIcon icon={faSearch} />
             </div>
@@ -34,79 +36,82 @@ const Navbar = ({ isLogged, isAdmin, isOpen, setIsOpen }) => {
               type="text"
               placeholder="Recherche..."
               className="navbar__searchform__input"
-              id="searchInput"
-
+              id="searchInputNavbar"
+              onChange={trackSearch}
             />
-          </div>
-          <a className="navbar__link__home" href="/">
-            <img className="navbar__link__home__icon" src={home} alt="icône accueil" />
-            Accueil
-          </a>
-          {/* TODO the following elements must only be displayed if the user is NOT logged in */}
-          {!isLogged
-          && (
-          <>
-            <a className="navbar__link__signin" href="/connexion">
-              Se connecter
-            </a>
-            <a className="navbar__link__signup" href="/insciption">
-              S'inscrire
-            </a>
-          </>
-          )}
-          {/* TODO the following elements must only be displayed if the user is logged in */}
-          {isLogged
-          && (
-          <>
-            <a className="navbar__link__profile" href="/profil/:slug">
-              <img className="navbar__link__profile__icon" href={profile} alt="icône profil" />
-              Mon profil
-            </a>
-            <a className="navbar__link__list" href="/profil/liste-de-courses">
-              <img className="navbar__link__list__icon" href={list} alt="icône liste de courses" />
-              Liste de courses
-            </a>
-            <a className="navbar__link__favorites" href="/profil/recettes-favorites">
-              <img className="navbar__link__favorites__icon" href={favorites} alt="icône favoris" />
-              Recettes favorites
-            </a>
-          </>
-          )}
-          {/* TODO the following elements must only be displayed if the user is logged in AND ADMIN */}
-          {isLogged && isAdmin
-          && (
-          <>
-            <a className="navbar__link__profile" href="/profil/:slug">
-              <img className="navbar__link__profile__icon" href={profile} alt="icône profil" />
-              Mon profil
-            </a>
-            <a className="navbar__link__adminrecipes" href="/admin/ajout-recette">
-              <img className="navbar__link__adminrecipes__icon" href={recipe} alt="icône recettes" />
-              Ajouter une recette
-            </a>
-            <a className="navbar__link__adminrecipes" href="/admin/modification-recette">
-              <img className="navbar__link__adminrecipes__icon" href={recipe} alt="icône recettes" />
-              Modifier/Supprimer une recette
-            </a>
-            <a className="navbar__link__adminlabels" href="/admin/gestion-labels">
-              <img className="navbar__link__adminlabels__icon" href={tag} alt="icône labels" />
-              Gérer les labels
-            </a>
-            <a className="navbar__link__adminusers" href="/admin/gestion-utilisateurs">
-              <img className="navbar__link__adminusers__icon" href={users} alt="icône utilisateurs" />
-              Gérer les utilisateurs
-            </a>
-          </>
-          )}
+          </form>
+            <NavLink to="/" className="navbar__link__home link__style">
+              <img className="navbar__link__icon" src={home} alt="icône accueil" />
+              Accueil
+            </NavLink>
+            {/* TODO the following elements must only be displayed if the user is NOT logged in */}
+            {!isLogged
+            && (
+            <>
+              <NavLink to="/connexion" className="navbar__link__signin link__style">
+                Se connecter
+              </NavLink>
+              <NavLink to="/insciption" className="navbar__link__signup button__style">
+                S'inscrire
+              </NavLink>
+            </>
+            )}
+            {/* TODO the following elements must only be displayed if the user is logged in */}
+            {isLogged
+            && (
+            <>
+              <NavLink to="/profil/:slug" className="navbar__link__profile link__style">
+                <img className="navbar__link__icon" src={profile} alt="icône profil" />
+                Mon profil
+              </NavLink>
+              <NavLink to="/profil/liste-de-courses" className="navbar__link__list link__style">
+                <img className="navbar__link__icon" src={list} alt="icône liste de courses" />
+                Liste de courses
+              </NavLink>
+              <NavLink to="/profil/recettes-favorites" className="navbar__link__favorites link__style">
+                <img className="navbar__link__icon" src={favorites} alt="icône favoris" />
+                Recettes favoritesNavLink
+              </NavLink>
+            </>
+            )}
+            {/* TODO the following elements must only be displayed if the user is logged in AND ADMIN */}
+            {isLogged && isAdmin
+            && (
+            <>
+              <NavLink to="/profil" className="navbar__link__profile link__style">
+                <img className="navbar__link__icon" src={profile} alt="icône profil" />
+                Mon profil
+              </NavLink>
+              <NavLink to="/admin/ajout-recette" className="navbar__link__adminrecipes link__style">
+                <img className="navbar__link__icon" src={recipe} alt="icône recettes" />
+                Ajouter une recette
+              </NavLink>
+              <NavLink to="/admin/modification-recette" className="navbar__link__adminrecipes link__style">
+                <img className="navbar__link__icon" src={recipe} alt="icône recettes" />
+                Modifier/Supprimer une recette
+              </NavLink>
+              <NavLink to="/admin/gestion-labels" className="navbar__link__adminlabels link__style">
+                <img className="navbar__link__icon" src={tag} alt="icône labels" />
+                Gérer les labels
+              </NavLink>
+              <NavLink to="/admin/gestion-utilisateurs" className="navbar__link__adminusers link__style">
+                <img className="navbar__link__icon" src={users} alt="icône utilisateurs" />
+                Gérer les utilisateurs
+              </NavLink>
+            </>
+            )}
+        </div>
           {/* The following elements are displayed for all users */}
-          <a className="navbar__link__contact" href="/contact">
-            <img className="navbar__link__contact__icon" src={contact} alt="icône contact" />
+        <div className="navbar__links__bottom">
+          <NavLink to="/contact" className="navbar__link__contact link__style">
+            <img className="navbar__link__icon" src={contact} alt="icône contact" />
             Contact
-          </a>
-          <a className="navbar__link__about" href="/a-propos">
-            <img className="navbar__link__about__icon" src={about} alt="icône à propos" />
+          </NavLink>
+          <NavLink to="/a-propos" className="navbar__link__about link__style">
+            <img className="navbar__link__icon" src={about} alt="icône à propos" />
             A propos
-          </a>
+          </NavLink>
+        </div>
         </div>
         }
       </>
@@ -125,8 +130,28 @@ const mapStateToProps = (state) => {
   }
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {}
-};
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  trackSearch: (event) => {
+    // console.log('Input email :', event.target.value);
+    dispatch({
+      type: 'EDIT_SEARCH_FIELD',
+      payload: {
+        search: event.target.value,
+      },
+    });
+  },
 
-export default connect(mapStateToProps, null)(Navbar);
+  handleSearch: (event) => {
+    event.preventDefault();
+    dispatch({
+      type: 'SEND_SEARCH_REQUEST',
+      redirect: ownProps.history.push
+    });
+  }
+});
+
+let container = connect(mapStateToProps, mapDispatchToProps)(Navbar);
+
+container = withRouter(container);
+
+export default container;
