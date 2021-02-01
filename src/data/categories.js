0437cc8ec7591b6abff7c417e968e22947e7554b
charlene1804 <jangozumi@gmail.com>
2021-02-01 18:16:17 +0100

    fix(src/data): fake data added back
    I deleted the fake data accidentally, it's back

---
+++ src/data/categories.js
@@ -0,0 +1,48 @@
+export default [
+  {
+    label: 'Produits Frais',
+    value: 'fresh_products',
+    id: '1',
+    items: [
+      {
+        id: 28,
+        quantity: 6,
+        unit: '',
+        name: 'oeufs',
+      },
+
+      {
+        id: 4,
+        quantity: 80,
+        unit: 'gr',
+        name: 'beurre',
+      },
+    ],
+  },
+  {
+    label: 'Epicerie',
+    value: 'condiments',
+    id: '2',
+    items: [
+      {
+        id: 1,
+        quantity: 375,
+        unit: 'gr',
+        name: 'farine',
+      },
+    ],
+  },
+  {
+    label: 'Liquides',
+    value: 'liquids',
+    id: '3',
+    items: [
+      {
+        id: 845,
+        quantity: 3,
+        unit: 'càs',
+        name: 'rhum',
+      },
+    ],
+  },
+];
