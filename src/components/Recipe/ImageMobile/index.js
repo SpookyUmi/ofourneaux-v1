023f23fbea26570a5958c915db77e783689ac73d
Corxys <jennblngr@gmail.com>
2021-02-05 11:51:38 +0100

    feat: addition of the season under the title of the recipe on the mobile version

--- src/components/Recipe/ImageMobile/index.js
+++ src/components/Recipe/ImageMobile/index.js
@@ -15,12 +15,14 @@ const Image = ({
   title,
   picture,
   tags,
+  seasons,
   status,
   isLogged,
   handleEditOrDeleteRecipe,
   sendRecipeInFavorites,
   favoritesRecipes,
   tagsData,
+  seasonsData,
 }) => {
   // by default the heart icon is empty
   let heart = heartLine;
@@ -94,6 +96,13 @@ const Image = ({
               </div>
             ))
           }
+          {
+            seasons.map((season) => (
+              <div key={season} className="recipe__infos__header__intro__tag">
+                {seasonsData.find((value) => value.id === season).name}
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>
@@ -107,12 +116,14 @@ Image.propTypes = {
   picture: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
   tags: PropTypes.array.isRequired,
+  seasons: PropTypes.array.isRequired,
   isLogged: PropTypes.bool.isRequired,
   handleEditOrDeleteRecipe: PropTypes.func.isRequired,
   sendRecipeInFavorites: PropTypes.func.isRequired,
   // eslint-disable-next-line react/require-default-props
   favoritesRecipes: PropTypes.array,
   tagsData: PropTypes.array.isRequired,
+  seasonsData: PropTypes.array.isRequired,
 };
 
 const mapStateToProps = (state) => ({
@@ -121,6 +132,7 @@ const mapStateToProps = (state) => ({
   isLogged: state.auth.isLogged,
   favoritesRecipes: state.user.favoritesRecipes,
   tagsData: state.app.tags,
+  seasonsData: state.app.seasons,
 });
 
 const mapDispatchToProps = (dispatch) => ({
